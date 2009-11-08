e8d7eb787a286d6f7e1da727ea54940b1e1918c4
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2009-11-08 07:34:46 +0100

    Fixes to recursion example, generated pdf

--- source/recursion.rb
+++ source/recursion.rb
@@ -1,13 +1,11 @@
 require 'base'
 
-class Treetop::Runtime::SyntaxNode
-  include Enumerable
-end
-
 list_grammar = <<TOP
 grammar List
   rule list
-    atom more_atoms:(',' atom)*
+    atom more_atoms:(',' atom)* {
+      def atoms; [atom] + more_atoms.elements.map{|m| m.atom}; end
+    }
   end
   rule atom
     '(' list ')' / number
@@ -18,15 +16,13 @@ grammar List
 end
 TOP
 
+Treetop::Runtime::SyntaxNode.send(:include, Enumerable)
 parser = Treetop.load_from_string(list_grammar).new
 if tree = parser.parse('(11,(12,13,14),(15,16))')
   reverser = lambda{|n|
-    atoms = [n.atom] + n.more_atoms.elements.map{|m| m.atom} 
-    atoms.map{|m|
+    n.atoms.map{|m|
       m.gsub("list", &reverser)
     }.reverse.join(",")
   }
   puts tree.gsub("list", &reverser)
-else
-  puts "buu"
 end
